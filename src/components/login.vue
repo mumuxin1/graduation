91c7bb9f64b63fb92411e481229ef96e97ed2212
mumuxin1 <1451560884@qq.com>
2018-04-17 18:23:44 +0800

    20180417

---
+++ src/components/login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="login">
+    <mt-field label="用户名" placeholder="请输入用户名" v-model="username"></mt-field>
+    <mt-field label="密码" placeholder="请输入密码" type="password" v-model="password"></mt-field>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+      username: '用户名',
+      password: '密码'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+// mint-ui vue移动端组件库
+import MintUI from 'mint-ui'
+Vue.use(MintUI)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
